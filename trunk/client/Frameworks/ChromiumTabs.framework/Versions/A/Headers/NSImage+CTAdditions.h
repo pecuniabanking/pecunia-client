b977df5e4c297c753411c9d1be4a556286a7a1ac
Mike Lischke <mike.lischke@gmail.com>
2012-10-06 20:43:16 +0200

    Branch is now pure 0.4 client.

--- trunk/client/Frameworks/ChromiumTabs.framework/Versions/A/Headers/NSImage+CTAdditions.h
+++
@@ -1,7 +0,0 @@
-#import <Cocoa/Cocoa.h>
-
-@interface NSImage (CTAdditions)
-+(NSImage*)imageWithPath:(NSString *)path;
-+(NSImage*)imageInAppOrCTFrameworkNamed:(NSString*)name;
-+(NSImage*)imageInFrameworkNamed:(NSString*)name;
-@end
--- trunk/client/Frameworks/CorePlot.framework/Versions/A/Headers/CPTPlatformSpecificDefines.h
+++
@@ -1,7 +0,0 @@
-#import <AppKit/AppKit.h>
-#import <Foundation/Foundation.h>
-
-///	@file
-
-typedef NSImage CPTNativeImage; ///< Platform-native image format.
-typedef NSEvent CPTNativeEvent; ///< Platform-native OS event.
