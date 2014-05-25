990f78d35bd96b91a720af4075bf1ed2c72c7a22
Mike Lischke <mike.lischke@gmail.com>
2014-05-25 17:54:11 +0200

    - ImportController:   - Did clean up.   - Implemented date ranges for imports. Fixed issue 234

--- Source/ImportController.h
+++ Source/ImportController.h
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2011, 2013, Pecunia Project. All rights reserved.
+ * Copyright (c) 2011, 2014, Pecunia Project. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
@@ -17,54 +17,12 @@
  * 02110-1301  USA
  */
 
-#import <Cocoa/Cocoa.h>
-
 @class BankQueryResult;
 @class BWGradientBox;
 
 @class ImportSettings;
-@class ProcessingPanel;
 
-@interface LinkTextField : NSTextField
-@end
-
-@interface FileEntry : NSObject
-
-@property (copy) NSString  *name;
-@property (copy) NSString  *path;
-@property (assign) BOOL    isFolder;
-@property (strong) NSImage *icon;
-
-@property (strong) NSMutableArray *children;
-
-@end
-
-@interface ImportController : NSWindowController {
-    IBOutlet NSArrayController *storedSettingsController;
-    IBOutlet NSPanel           *newSettingsNameSheet;
-    IBOutlet ProcessingPanel   *processingSheet;
-    IBOutlet NSTextField       *settingsNameField;
-    IBOutlet NSTextField       *dateFormatLinkLabel;
-    IBOutlet BWGradientBox     *separatorGradient;
-    IBOutlet NSArrayController *accountsController;
-    IBOutlet NSPopUpButton     *decimalSeparatorPopupButton;
-    IBOutlet NSPopUpButton     *fieldSeparatorPopupButton;
-    IBOutlet NSTextField       *customFieldSeparator;
-    IBOutlet NSTextField       *customFieldSeparatorLabel;
-
-@private
-    NSManagedObjectContext *managedObjectContext;
-    NSMutableSet           *fileNames;
-
-    NSFont            *textFont;
-    NSString          *currentFile;
-    NSArray           *currentLines;
-    NSDateFormatter   *dateFormatter;
-    NSNumberFormatter *numberFormatter;
-
-    NSMutableArray *parsedValues;
-    BOOL           updating;
-}
+@interface ImportController : NSWindowController
 
 @property (strong) IBOutlet BWGradientBox *backgroundGradient;
 @property (strong) IBOutlet BWGradientBox *topGradient;
@@ -84,11 +42,5 @@
 @property (strong) BankQueryResult *importResult;
 @property (strong) NSMutableArray  *importValues;
 @property (assign) BOOL            stopProcessing;
-
-- (IBAction)cancel: (id)sender;
-- (IBAction)startProcessing: (id)sender;
-
-- (IBAction)addRemoveFile: (id)sender;
-- (IBAction)settingSelectionChanged: (id)sender;
 
 @end
