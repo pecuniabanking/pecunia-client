cae0d9fb81c1798265225bcbb166be335d94a78c
Mike Lischke <mike@lischke-online.de>
2015-06-14 16:38:11 +0200

    Fixed formatting.

--- Plugins/dkb.js
+++ Plugins/dkb.js
@@ -52,11 +52,11 @@ function canHandle(account, bankCode) {
 }
 
 function logOut() {
-	logger.logDebug("Starting log out...");
-	currentState = 99;
-	userName = "";
-	thePassword = "";
-	webClient.URL = dkbLogoutURL;
+    logger.logDebug("Starting log out...");
+    currentState = 99;
+    userName = "";
+    thePassword = "";
+    webClient.URL = dkbLogoutURL;
 }
 
 // Cache variables to allow for async loading.
@@ -67,29 +67,29 @@ var endAt;
 var results = [];
 
 function getStatements(user, bankCode, password, from, to, creditCardNumbers) {
-	numbers = creditCardNumbers;
-	startFrom = from;
-	endAt = to;
-	results = [];
-	singleStep = false;
+    numbers = creditCardNumbers;
+    startFrom = from;
+    endAt = to;
+    results = [];
+    singleStep = false;
 
-	if (user == "") {
-		logger.logError("Login: user name empty");
-		return false;
-	}
-	
-	logger.logDebug("Statements for user: " + user);
-	logger.logDebug("From: " + from);
-	logger.logDebug("To: " + to);
-	logger.logDebug("Credit cards: " + creditCardNumbers);
+    if (user == "") {
+        logger.logError("Login: user name empty");
+        return false;
+    }
 
-	userName = user;
-	thePassword = password;
-	currentState = 11;
-	webClient.callback = navigationCallback;
-	webClient.URL = dkbBaseURL;
+    logger.logDebug("Statements for user: " + user);
+    logger.logDebug("From: " + from);
+    logger.logDebug("To: " + to);
+    logger.logDebug("Credit cards: " + creditCardNumbers);
 
-	return true;
+    userName = user;
+    thePassword = password;
+    currentState = 11;
+    webClient.callback = navigationCallback;
+    webClient.URL = dkbBaseURL;
+
+    return true;
 }
 
 // Called from the plugin worker, e.g. after navigating to a new page or when executing a single step.
@@ -98,27 +98,27 @@ function getStatements(user, bankCode, password, from, to, creditCardNumbers) {
 var singleStep = false;
 
 function navigationCallback(doStep) {
-	if (singleStep) {
-		// We executed a step and hold the state machine here.
-		singleStep = false;
-		return;
-	}
-	
-	if (doStep) {
-		// Got the command to run only one step. Continue with the current state
-		// but stop the state machine when this is finished.
-		singleStep = true;
-	}
-	
-	logger.logDebug("State machine at: " + currentState);
-	try {
-		switch (currentState) {
-			case 1, 11: // 1 for pure login, 11 for login + statements.
-				startLogin();
-				break;
+    if (singleStep) {
+        // We executed a step and hold the state machine here.
+        singleStep = false;
+        return;
+    }
 
-			case 12: // After log in state. If all is fine collect statements.
-				logger.logDebug("URL after login: " + webClient.URL);
+    if (doStep) {
+        // Got the command to run only one step. Continue with the current state
+        // but stop the state machine when this is finished.
+        singleStep = true;
+    }
+
+    logger.logDebug("State machine at: " + currentState);
+    try {
+        switch (currentState) {
+            case 1, 11: // 1 for pure login, 11 for login + statements.
+                startLogin();
+                break;
+
+            case 12: // After log in state. If all is fine collect statements.
+                logger.logDebug("URL after login: " + webClient.URL);
 
                 // Check if we were sent back to the login form.
                 var formLogin = webClient.mainFrame.document.forms.item(0);
@@ -133,103 +133,103 @@ function navigationCallback(doStep) {
                     return;
                 }
 
-				navigateToCreditCardPage();
-				break;
+                navigateToCreditCardPage();
+                break;
 
-			case 13:
-				logger.logDebug("URL after credit card menu selection: " + webClient.URL);
-				startCreditCardLoop();
-				break;
-				
-			case 14: // Arrived at list of statements. Start CSV export and collect results.
-				currentState = 15;
-				webClient.URL = dkbCsvURL;
-				break;
+            case 13:
+                logger.logDebug("URL after credit card menu selection: " + webClient.URL);
+                startCreditCardLoop();
+                break;
 
-			case 15:
-				convertCsvToResult();
-				break;
-				
-			case 16: // Continue credit card loop.
-				readNextCreditCard();
-				break;
-				
-			case 99:
-				logger.logDebug("Log out done");
-				// Fall through;
-			default:
-				currentState = 0;
-				webClient.callback = null; // Unregister ourselve. We are done.
-				logger.logDebug("Plugin invocation done");
-			}
-		} catch (err) {
-			if (currentState != 99) // If exception not triggered by logout.
-				logOut();
-			throw err;
-		}
+            case 14: // Arrived at list of statements. Start CSV export and collect results.
+                currentState = 15;
+                webClient.URL = dkbCsvURL;
+                break;
+
+            case 15:
+                convertCsvToResult();
+                break;
+
+            case 16: // Continue credit card loop.
+                readNextCreditCard();
+                break;
+
+            case 99:
+                logger.logDebug("Log out done");
+                // Fall through;
+            default:
+                currentState = 0;
+                webClient.callback = null; // Unregister ourselve. We are done.
+                logger.logDebug("Plugin invocation done");
+        }
+    } catch (err) {
+        if (currentState != 99) // If exception not triggered by logout.
+            logOut();
+        throw err;
+    }
 }
 
 function startLogin() {
-	var LoginURL = webClient.URL;
-	logger.logDebug("At URL: " + LoginURL + ", triggering login process");
-	if (LoginURL.indexOf("/portal") !== -1) {
-		logger.debug("Running new portal login");
-		/* TODO
-		var formLogin = pageLogin.getFirstByXPath("//form[@class='anmeldung']");
-		logger.logDebug("formLogin: " + formLogin + ", name: " + formLogin.name);
-		formLogin.getFirstByXPath("//input[@maxlength='16']").setValueAttribute(ResponseLogin);
-		formLogin.getFirstByXPath("//input[@type='password']").setValueAttribute(ResponsePasswort);
-		var submitLogin = formLogin.getInputByValue("Anmelden");
-		*/
+    var LoginURL = webClient.URL;
+    logger.logDebug("At URL: " + LoginURL + ", triggering login process");
+    if (LoginURL.indexOf("/portal") !== -1) {
+        logger.debug("Running new portal login");
+        /* TODO
+         var formLogin = pageLogin.getFirstByXPath("//form[@class='anmeldung']");
+         logger.logDebug("formLogin: " + formLogin + ", name: " + formLogin.name);
+         formLogin.getFirstByXPath("//input[@maxlength='16']").setValueAttribute(ResponseLogin);
+         formLogin.getFirstByXPath("//input[@type='password']").setValueAttribute(ResponsePasswort);
+         var submitLogin = formLogin.getInputByValue("Anmelden");
+         */
 
-	} else if (LoginURL.indexOf("/dkb") !== -1) {
-		logger.logDebug("Running old standard login");
-		var formLogin = webClient.mainFrame.document.forms.item(0);
+    } else if (LoginURL.indexOf("/dkb") !== -1) {
+        logger.logDebug("Running old standard login");
+        var formLogin = webClient.mainFrame.document.forms.item(0);
 
-		logger.logDebug("formLogin: " + formLogin + ", name: " + formLogin.name + ", action: " + formLogin.action);
-		formLogin.elements.namedItem("j_username").setValue(userName);
-		formLogin.elements.namedItem("j_password").setValue(thePassword);
-		var submitLogin = formLogin.elements.namedItem("buttonlogin");
-		logger.logDebug("submit button: " + submitLogin + ", id: " + submitLogin.idName);
+        logger.logDebug("formLogin: " + formLogin + ", name: " + formLogin.name + ", action: " + formLogin.action);
+        formLogin.elements.namedItem("j_username").setValue(userName);
+        formLogin.elements.namedItem("j_password").setValue(thePassword);
+        var submitLogin = formLogin.elements.namedItem("buttonlogin");
+        logger.logDebug("submit button: " + submitLogin + ", id: " + submitLogin.idName);
 
-	} else if (LoginURL.indexOf("wartung") !== -1) {
-		logger.logError("DKB site currently not available");
-		currentState = 0;
-		webClient.callback = null;
-		return;
-		
-		// Might be good to use a similar response handling as is used in Hibiscus, e.g.
-		//HibiscusScripting_DKBVisa_checkResponse(pageLoginXML, pageLogin, "Login", monitor);
-	} else {
-		throw "Invalid login URL. Please inform the developers.";
-	};
+    } else if (LoginURL.indexOf("wartung") !== -1) {
+        logger.logError("DKB site currently not available");
+        currentState = 0;
+        webClient.callback = null;
+        return;
 
-	currentState = (currentState == 1) ? 2 : 12;
-	submitLogin.click();
-	logger.logInfo("Login process triggered");
+        // Might be good to use a similar response handling as is used in Hibiscus, e.g.
+        //HibiscusScripting_DKBVisa_checkResponse(pageLoginXML, pageLogin, "Login", monitor);
+    } else {
+        throw "Invalid login URL. Please inform the developers.";
+    };
+
+    currentState = (currentState == 1) ? 2 : 12;
+    submitLogin.click();
+    logger.logInfo("Login process triggered");
 }
 
 function navigateToCreditCardPage() {
-	logger.logDebug("Navigating to credit card page");
-	
-	var url = webClient.URL;
-	if (url.indexOf("/portal") !== -1) {
-		// todo: url = dkbBaseURL + ActiveContent.getAnchorByText("Kreditkartenums\u00E4tze").getHrefAttribute();
-	} else if (url.indexOf("/dkb") !== -1) {
-		url = dkbCardSelectionURL;
-	} else {
-		throw "Unknown URL, couldn't get credit card overview";
-	};
-	
-	currentState = 13;
-	webClient.URL = url;
+    logger.logDebug("Navigating to credit card page");
+
+    var url = webClient.URL;
+    if (url.indexOf("/portal") !== -1) {
+        // todo: url = dkbBaseURL + ActiveContent.getAnchorByText("Kreditkartenums\u00E4tze").getHrefAttribute();
+    } else if (url.indexOf("/dkb") !== -1) {
+        url = dkbCardSelectionURL;
+    } else {
+        throw "Unknown URL, couldn't get credit card overview";
+    };
+
+    currentState = 13;
+    webClient.URL = url;
 }
 
 var currentCreditCardIndex;
 
 function startCreditCardLoop() {
-	currentCreditCardIndex = -1;
-	readNextCreditCard();
+    currentCreditCardIndex = -1;
+    readNextCreditCard();
 }
 
 function padDatePart(i) {
@@ -237,83 +237,83 @@ function padDatePart(i) {
 }
 
 function readNextCreditCard() {
-	if (++currentCreditCardIndex >= numbers.length) {
-		// We are done.
+    if (++currentCreditCardIndex >= numbers.length) {
+        // We are done.
         webClient.resultsArrived(results);
-		logOut();
-		return;
-	};
-	
-	var url = webClient.URL;
-	if (url.indexOf("/portal") !== -1) {
-		// todo
-	} else if (url.indexOf("/dkb") !== -1) {
-		for (var i = 0; i < webClient.mainFrameDocument.forms.length; ++i) {
-			var form = webClient.mainFrameDocument.forms.item(i);
-			if (form.name == "form772007528_1")
-				break;
-		}
-		//var form = webClient.mainFrameDocument.forms.namedItem("form772007528_1"); namedItem doesn't work on forms it seems.
-		var creditCardSelector = form.elements.namedItem("slCreditCard");
-	} else {
-		throw "Unknown URL, couldn't get credit card overview";
-	};
+        logOut();
+        return;
+    };
 
-	logger.logDebug("CC selector: " + creditCardSelector);
-	var ccNumber = numbers[currentCreditCardIndex].substr(0, 4) + "********" + numbers[currentCreditCardIndex].substr(numbers[currentCreditCardIndex].length - 4, 4);
-	logger.logDebug("Looking for credit card entry: " + ccNumber);
-	
-	var list = creditCardSelector.options;
-	for (var i = 0; i < list.length; ++i) {
-		var creditCardEntry = list.item(i);
-		logger.logVerbose("CC item: " + creditCardEntry.text);
-		if (creditCardEntry.text.substr(0, 16) == ccNumber) {
-			creditCardEntry.selected = true;
-			ccNumber = "***";
-			break;
-		};
-	};
-	
-	if (ccNumber != "***") {
-		// Credit card not found.
-		logger.logError("Couldn't find credit card in list (" + ccNumber + ")");
-		readNextCreditCard();
-		return;
-	};
-	
-	// Somehow I cannot get getElementsByName and getElementById to work as expected.
-	// logger.logDebug("Search element: " + webClient.mainFrameDocument.getElementById("searchPeriod.0"));
-	for (var i = 0; i < form.elements.length; ++i) {
-		var element = form.elements.item(i);
-		if (element.idName == "searchPeriod.0") {
-			logger.logDebug("searchPeriod: " + element);
-			element.checked = true; // Select date range input.
-			break;
-		}
-	}
-	
-	var postingDate = form.elements.namedItem("postingDate");
-	logger.logDebug("From posting element: " + postingDate);
-	var toPostingDate = form.elements.namedItem("toPostingDate");
-	logger.logDebug("To posting element: " + toPostingDate);
+    var url = webClient.URL;
+    if (url.indexOf("/portal") !== -1) {
+        // todo
+    } else if (url.indexOf("/dkb") !== -1) {
+        for (var i = 0; i < webClient.mainFrameDocument.forms.length; ++i) {
+            var form = webClient.mainFrameDocument.forms.item(i);
+            if (form.name == "form772007528_1")
+                break;
+        }
+        //var form = webClient.mainFrameDocument.forms.namedItem("form772007528_1"); namedItem doesn't work on forms it seems.
+        var creditCardSelector = form.elements.namedItem("slCreditCard");
+    } else {
+        throw "Unknown URL, couldn't get credit card overview";
+    };
 
-	postingDate.setValue(padDatePart(startFrom.getDate()) + "." + padDatePart(startFrom.getMonth() + 1) + "." + startFrom.getFullYear());
-	toPostingDate.setValue(padDatePart(endAt.getDate()) + "." + padDatePart(endAt.getMonth() + 1) + "." + endAt.getFullYear());
-	
-	logger.logDebug("Date range: " + postingDate.value() + " .. " + toPostingDate.value());
-	
-	currentState = 14;
-	var button = form.elements.namedItem("searchbutton");
-	button.click();
+    logger.logDebug("CC selector: " + creditCardSelector);
+    var ccNumber = numbers[currentCreditCardIndex].substr(0, 4) + "********" + numbers[currentCreditCardIndex].substr(numbers[currentCreditCardIndex].length - 4, 4);
+    logger.logDebug("Looking for credit card entry: " + ccNumber);
+
+    var list = creditCardSelector.options;
+    for (var i = 0; i < list.length; ++i) {
+        var creditCardEntry = list.item(i);
+        logger.logVerbose("CC item: " + creditCardEntry.text);
+        if (creditCardEntry.text.substr(0, 16) == ccNumber) {
+            creditCardEntry.selected = true;
+            ccNumber = "***";
+            break;
+        };
+    };
+
+    if (ccNumber != "***") {
+        // Credit card not found.
+        logger.logError("Couldn't find credit card in list (" + ccNumber + ")");
+        readNextCreditCard();
+        return;
+    };
+
+    // Somehow I cannot get getElementsByName and getElementById to work as expected.
+    // logger.logDebug("Search element: " + webClient.mainFrameDocument.getElementById("searchPeriod.0"));
+    for (var i = 0; i < form.elements.length; ++i) {
+        var element = form.elements.item(i);
+        if (element.idName == "searchPeriod.0") {
+            logger.logDebug("searchPeriod: " + element);
+            element.checked = true; // Select date range input.
+            break;
+        }
+    }
+
+    var postingDate = form.elements.namedItem("postingDate");
+    logger.logDebug("From posting element: " + postingDate);
+    var toPostingDate = form.elements.namedItem("toPostingDate");
+    logger.logDebug("To posting element: " + toPostingDate);
+
+    postingDate.setValue(padDatePart(startFrom.getDate()) + "." + padDatePart(startFrom.getMonth() + 1) + "." + startFrom.getFullYear());
+    toPostingDate.setValue(padDatePart(endAt.getDate()) + "." + padDatePart(endAt.getMonth() + 1) + "." + endAt.getFullYear());
+
+    logger.logDebug("Date range: " + postingDate.value() + " .. " + toPostingDate.value());
+
+    currentState = 14;
+    var button = form.elements.namedItem("searchbutton");
+    button.click();
 }
 
 function convertCsvToResult() {
-	
-	logger.logDebug("Converting results");
-	var lines = webClient.mainFrameDocument.body.innerText.split("\n");
 
-	var statements = [];
-	var headers = ["final", "valutaDate", "date", "transactionText", "value", "originalValue"];
+    logger.logDebug("Converting results");
+    var lines = webClient.mainFrameDocument.body.innerText.split("\n");
+
+    var statements = [];
+    var headers = ["final", "valutaDate", "date", "transactionText", "value", "originalValue"];
 
     // Balance value is in prefix lines.
     var balance = "0 EUR";
@@ -333,39 +333,39 @@ function convertCsvToResult() {
         }
     }
 
-	for (var i = 8; i < lines.length; ++i) {
-		var statement = {};
-		var currentLine = lines[i].split(";");
+    for (var i = 8; i < lines.length; ++i) {
+        var statement = {};
+        var currentLine = lines[i].split(";");
 
         // This field seems not what we understand from it. Even though it might say the value
         // is not yet processed it is actually already contained in the balance.
         statement["final"] = true; //unquote(currentLine[0]) == "Ja" ? true : false;
-
+        
         var parts = unquote(currentLine[1]).split('.');
         statement["valutaDate"] = new Date(parts[2], parts[1] - 1, parts[0]);
-
+        
         parts = unquote(currentLine[2]).split('.');
         statement["date"] = new Date(parts[2], parts[1] - 1, parts[0]);
-
+        
         for (var j = 3; j < headers.length; ++j) {
-			statement[headers[j]] = unquote(currentLine[j]);
-		}
-
-		statements.push(statement);
-	}
-	
-	var result = {
+            statement[headers[j]] = unquote(currentLine[j]);
+        }
+        
+        statements.push(statement);
+    }
+    
+    var result = {
         "isCreditCard": true,
         "account": numbers[currentCreditCardIndex],
         "balance": balance,
         "statements": statements,
         "lastSettleDate": lastSettleDate
     };
-	results.push(result);
-
-	logger.logDebug("Done converting")
-	currentState = 16;
-	webClient.goBack(); // Back to credit card selection.
+    results.push(result);
+    
+    logger.logDebug("Done converting")
+    currentState = 16;
+    webClient.goBack(); // Back to credit card selection.
 }
 
 function sleepFor(sleepDuration) { // Debugging helper.
